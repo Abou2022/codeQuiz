25335a213a10a3feeb85a06f38bfbd3443efd6b3
Bakary Sylla <syllabakary2002@gmail.com>
2022-04-05 21:46:50 -0700

    WIP

---
+++ script.js
@@ -0,0 +1,44 @@
+
+//Quiz question
+//set up my variable(arrays)
+//creat a object for question all question
+var startBtn = document.querySelector("#Start");
+var timeLeft = 2;
+var wordPara = document.querySelector("#wordPara");
+
+var questionArray = [
+    {
+    questionTitle: "What is JavaScript",
+   possibleAnswers: ['Programming Language','type of video','dictionnary'],
+    correctAnswer: "Programming Language"
+    }
+];//timer
+//diplay the question
+//display the question on the top of the screem
+//probable Answer
+
+
+questionArray[0].questionTitle
+
+
+
+console.log(questionArray[0].questionTitle)
+    //genered 
+    document.createElement
+    //asket question by order
+    //Make varibles into the arrays
+    //When quiz start,  time start 
+
+    
+    //if start button click, question appeart
+    //each question have 3 choise
+    //when answer is submit 
+    //next question appear
+    //when question incorrection question is submit time decrease(subract)
+    //if question = 0 Game is over
+    //or if time = Game is over
+    //if game is over we can save the time
+
+    //start the timer display question ; Dom manipulation (qureyselctor, attribute...)
+    //
+
